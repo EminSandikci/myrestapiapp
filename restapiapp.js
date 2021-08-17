7c2a13a7a02a9ca0413d7f21f822b7ec5fccf413
Test Project <test@asdasd.asd>
2021-08-17 20:23:41 +0300

    add frontend page

--- restapiapp.js
+++ restapiapp.js
@@ -5,12 +5,12 @@ const { userRouter } = require('./routers/user')
 
 require('dotenv/config')
 
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, (err) => {
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (!err) console.log('db connect..')
 })
 
 app.use(userRouter)
 
-app.listen(3000,(err)=>{
-    if(!err) console.log('listining 3000 port..')
+app.listen(3000, (err) => {
+    if (!err) console.log('listining 3000 port..')
 })
